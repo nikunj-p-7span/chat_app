85c70e5adbeead1f1a23b471296905975fb28c30
nikunj-p-7span <nikunj.p.7span.com>
2023-12-12 15:58:44 +0530

    -

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:chat_app/module/chat/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
 
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,4 +26,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
